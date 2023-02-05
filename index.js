3763f0dcdedcea4a941d985be87645826d02584b
ImJustNon <Im_Just_Non@hotmail.com>
2023-02-05 19:34:33 +0700

    Advance console.log

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const { console } = require('./print.js');
+
+
+
+console.log("Normal Hello World!");
+console.document.write.System.out.println.print.echo.printf.cout.print_r.NSLog.puts.fprintf.Console.WriteLine.append.disp.cat.MsgBox.Response.write.System.Console.WriteLine.fmt.Println.writeln.log("Advance Hello World!");
---
+++ print.js
@@ -0,0 +1,63 @@
+module.exports = {
+    console: {
+        document: {
+            write: {
+                System: {
+                    out: {
+                        println: {
+                            print: { 
+                                echo: {
+                                    printf: {
+                                        cout: {
+                                            print_r: {
+                                                NSLog: {
+                                                    puts: {
+                                                        fprintf: {
+                                                            Console: {
+                                                                WriteLine: {
+                                                                    append: {
+                                                                        disp: {
+                                                                            cat: {
+                                                                                MsgBox: {
+                                                                                    Response: {
+                                                                                        write: {
+                                                                                            System: {
+                                                                                                Console: {
+                                                                                                    WriteLine: {
+                                                                                                        fmt: {
+                                                                                                            Println: {
+                                                                                                                writeln: {
+                                                                                                                    log: console.log,
+                                                                                                                },
+                                                                                                            },
+                                                                                                        },
+                                                                                                    },
+                                                                                                },
+                                                                                            },
+                                                                                        },
+                                                                                    },
+                                                                                },
+                                                                            },
+                                                                        },
+                                                                    },
+                                                                },
+                                                            },
+                                                        },
+                                                    },
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+        log: console.log,
+    },
+}
+
+
+
